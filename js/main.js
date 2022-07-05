19b9573b4402e2145b29271df230ab2be8194cbf
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2022-07-06 01:04:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "images/moon.png";
+    icon.src = "Images/moon.png";
   } else {
-    icon.src = "images/sun.png";
+    icon.src = "Images/sun.png";
   }
 };
