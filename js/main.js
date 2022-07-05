ee432e6b0361c45b7216d1b8087c54b966f85b0c
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2022-07-06 01:27:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ var icon = document.getElementById("icon");
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
+  icon.src = "Images/moon.png"
   if (document.body.classList.contains("dark-theme")) {
     icon.src = "Images/moon.png";
   } else {
