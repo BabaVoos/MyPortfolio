3478d5c08cf5379e0201e0e881b62094b15a4138
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2022-07-06 00:53:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let myBtn = document.querySelector(".slider");
+
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+};
+
+myBtn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "images/moon.png";
+  } else {
+    icon.src = "images/sun.png";
+  }
+};
